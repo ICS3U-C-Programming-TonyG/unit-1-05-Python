9eb49fc10c4b16a86ca1981530383fce637b18b6
TONY GAO <TONY.GAO@STU.OCSB.CA>
2025-02-19 17:46:54 +0000

    describes the area of a rectangle

---
+++ area_per_rect.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+# Created By: Tony G
+
+# Date: 2025-02-19
+
+# Shows the dimensions of a 2d rectangle
+
+
+def main():
+    print("If a rectangle has the following dimensions:")
+    print("10cm x 10cm")
+    print("100")
+
+
+if __name__ == "__main__":
+    main()
